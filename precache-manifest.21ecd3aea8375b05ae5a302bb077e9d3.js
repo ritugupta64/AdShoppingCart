19645690b486c9b07a60071cf068b2d2fd3b816e
ritugupta64 <gritu64@gmail.com>
2019-10-17 19:28:30 +0530

    Updates

---
+++ precache-manifest.21ecd3aea8375b05ae5a302bb077e9d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd19083ebd3bc7ca362478c3fe105161",
+    "url": "/AdShoppingCart/index.html"
+  },
+  {
+    "revision": "0c94f131280f5e67699f",
+    "url": "/AdShoppingCart/static/css/main.e70cd2c6.chunk.css"
+  },
+  {
+    "revision": "69b4958557aff470166d",
+    "url": "/AdShoppingCart/static/js/2.f4a6802a.chunk.js"
+  },
+  {
+    "revision": "0c94f131280f5e67699f",
+    "url": "/AdShoppingCart/static/js/main.87e6b746.chunk.js"
+  },
+  {
+    "revision": "cc78f2a7b5bbb862d80e",
+    "url": "/AdShoppingCart/static/js/runtime-main.bb570178.js"
+  }
+]);
